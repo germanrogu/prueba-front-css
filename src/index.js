8605952ac41c0a23b5023ae128302fd66d57650d
German <german.rodriguezgu@gmail.com>
2022-01-28 23:38:34 -0500

    :lipstick:

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import ReactDom from 'react-dom'
 
 import { App } from './App'
 
-import './fonts/Circular-Std-Bold.otf'
-import './fonts/Circular-Std-Book.otf'
-import './fonts/Circular-Std-Medium.otf'
 
 ReactDom.render(
   <App />,
